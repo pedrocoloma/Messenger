ee21c064563300eb171570bb0f311d03d180c529
Pedro <pfcaraujo@uolinc.com>
2022-05-17 11:47:42 -0300

    Project organization

--- Messenger/Controllers/ConversationsViewController.swift
+++ Messenger/Controllers/ConversationsViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ConversationsViewController.swift
 //  Messenger
 //
 //  Created by Pedro Felipe Coloma de Araujo on 17/05/22.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ConversationsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
